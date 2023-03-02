5c8930cc65471ad5b7175258928fa57d352e20ec
bigbyto <bigbyto@Bigbytos-iMac.local>
2023-03-02 14:07:43 +0800

    * 优化构建脚本

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [[ ! -d ./build ]]; then
   mkdir ./build
 fi
 
-version=$(cat appcast.json | grep '"version"' | awk -F: 'NR==1 {print $2}' | awk -F\" '{print $2}')
+version=$(cat appcast.json | grep '"version"' | awk -F\" 'NR==1 {print $4}')
 echo "Latest version number: $version"
 cd src
 zip -r ../build/bob-plugin-youdao-dict_v$version.bobplugin . * 
